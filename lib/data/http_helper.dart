9db0f3a5c1ff29d9209cb98aaf3408f6d0b4ba60
mguppy <mguppy34@gmail.com>
2021-12-15 09:20:21 -0600

    weather info populating

---
+++ lib/data/http_helper.dart
@@ -0,0 +1,22 @@
+// import 'package:hello_world_2/data/weather.dart';
+import '../data/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HttpHelper {
+  // var currentweatherapiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearched + '&appid=' + apiKey;
+
+  final String authority = 'api.openweathermap.org';
+  final String path = 'data/2.5/weather';
+  final String apiKey = 'be12c130488a49e9f35617a382464679';
+
+  Future<Weather> getWeather(String location) async {
+    Map<String, dynamic> paramaters = {'q': location, 'appId': apiKey};
+    Uri uri = Uri.https(authority, path, paramaters);
+    http.Response result = await http.get(uri);
+    Map<String, dynamic> data = json.decode(result.body);
+    Weather weather = Weather.fromJson(data);
+
+    return weather;
+  }
+}
